6abd8785ab4d622f4578757c48b19b57f8ecb317
zjhmale <zjhmale@gmail.com>
2015-10-14 20:37:52 +0800

    handle file not found in bucket error

--- src/hermione/handler.clj
+++ src/hermione/handler.clj
@@ -8,6 +8,8 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
   (:import (hermione DigestUtil)
            (com.qiniu.util Auth)
+           (com.qiniu.storage BucketManager)
+           (com.qiniu.common QiniuException)
            (java.io File)
            (java.text SimpleDateFormat)))
 
@@ -15,7 +17,9 @@
 (def base-path (get-property (get-config "hermione") "hermione" "basepath"))
 (def ak (get-property (get-config "hermione") "hermione" "ak"))
 (def sk (get-property (get-config "hermione") "hermione" "sk"))
+(def bucket (get-property (get-config "hermione") "hermione" "bucket"))
 (def auth (Auth/create ak sk))
+(def bucket-manager (BucketManager. auth))
 
 (def sdf (SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ss"))
 
@@ -28,11 +32,21 @@
   [name]
   (str base-path name))
 
+(defn file-exists?
+  [name]
+  (try
+    (do (.stat bucket-manager bucket name)
+        true)
+    (catch QiniuException _ false)))
+
 (defn write-file-s
   [name]
-  (clojure.java.io/copy
-    (:body (client/get (gen-url name) {:as :stream}))
-    (File. (gen-path name))))
+  (try
+    (clojure.java.io/copy
+      (:body (client/get (gen-url name) {:as :stream}))
+      (File. (gen-path name))
+      true)
+    (catch Exception _ false)))
 
 (defn response [data & [status]]
   {:status  (or status 200)
@@ -42,16 +56,18 @@
 
 (defroutes app-routes
   (GET "/api/wopi/files/:name" [name]
-    (do (write-file-s name)
-        (let [file (File. (gen-path name))
-              size (.length file)
-              sha256 (DigestUtil/getFileHash "SHA-256" (gen-path name))
-              version (.format sdf (.lastModified file))]
-          (response {:BaseFileName name
-                     :OwnerId      "admin"
-                     :Size         size
-                     :SHA256       sha256
-                     :Version      version}))))
+    (if (file-exists? name)
+      (do (write-file-s name)
+          (let [file (File. (gen-path name))
+                size (.length file)
+                sha256 (DigestUtil/getFileHash "SHA-256" (gen-path name))
+                version (.format sdf (.lastModified file))]
+            (response {:BaseFileName name
+                       :OwnerId      "admin"
+                       :Size         size
+                       :SHA256       sha256
+                       :Version      version})))
+      (response {:error "file not found"})))
   (GET "/api/wopi/files/:name/contents" [name]
     (let [filepath (gen-path name)
           resp (file-response filepath)]
