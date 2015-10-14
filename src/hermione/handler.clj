aebe9d99c8d43daebbc51bcc5764c275498357d5
zjhmale <zjhmale@gmail.com>
2015-10-14 11:42:41 +0800

    finish getfileinfo api

--- src/hermione/handler.clj
+++ src/hermione/handler.clj
@@ -1,32 +1,54 @@
 (ns hermione.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.middleware.json :refer [wrap-json-response]]
             [clj-http.client :as client]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
   (:import (hermione DigestUtil)
            (java.io File)
            (java.text SimpleDateFormat)))
 
-(def test-url "http://7xj0bp.com1.z0.glb.clouddn.com/test.docx")
-(def test-path "/Users/zjh/Downloads/test.docx")
+(def base-url "http://7xj0bp.com1.z0.glb.clouddn.com/")
+(def base-path "/Users/zjh/Downloads/")
 
-(defn write-file-s []
+(def sdf (SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ss"))
+
+(defn gen-url
+  [name]
+  (str base-url name))
+
+(defn gen-path
+  [name]
+  (str base-path name))
+
+(defn write-file-s
+  [name]
   (clojure.java.io/copy
-    (:body (client/get test-url {:as :stream}))
-    (File. test-path)))
+    (:body (client/get (gen-url name) {:as :stream}))
+    (File. (gen-path name))))
+
+(defn response [data & [status]]
+  {:status  (or status 200)
+   :headers {"Content-Type"                 "application/json;charset=utf-8"
+             "Access-Control-Allow-Methods" "GET, POST, PUT, DELETE, OPTIONS"}
+   :body    data})
 
 (defroutes app-routes
   (GET "/api/wopi/files/:name" [name]
-    (do
-      (write-file-s)
-      (let [file (File. test-path)
-            sdf (SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ss")]
-        (println "length -> " (.length file))
-        (println "last modify -> " (.format sdf (.lastModified file))))
-      (println "sha256 -> " (DigestUtil/getFileHash "SHA-256" test-path))
-      "hehe"))
+    (do (write-file-s name)
+        (let [file (File. (gen-path name))
+              size (.length file)
+              sha256 (DigestUtil/getFileHash "SHA-256" (gen-path name))
+              version (.format sdf (.lastModified file))]
+          (response {:BaseFileName name
+                     :OwerId       "admin"
+                     :Size         size
+                     :SHA256       sha256
+                     :Version      version}))))
   (GET "/api/wopi/files/:name/contents" [name] (str name " file contents"))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (wrap-defaults site-defaults)
+      wrap-json-response))
